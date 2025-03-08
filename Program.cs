89bcc8102d7c63ff253c566537be1d830d6acfa6
iagooteles <iagooteles@gmail.com>
2025-03-08 15:16:27 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+BankAccount account1 = new BankAccount("iago", 150);
+BankAccount account2 = new BankAccount("bilas", 3000);
+
+Console.WriteLine("Saldo antes do deposito: " + account1.Balance);
+account1.Deposit(300);
+Console.WriteLine("Saldo depois do deposito: " + account2.Balance);
+
+class ConsoleLogger
+{
+    
+}
+
+interface ILogger
+{
+    void Log(string message);
+}
+
+class BankAccount 
+{
+    private string name;
+    private decimal balance;
+
+    public decimal Balance 
+    { 
+        get { return balance; }
+    }
+
+    public BankAccount(string name, decimal balance) {
+        if (string.IsNullOrWhiteSpace(name)) 
+        {
+            throw new Exception("Nome invalido.");
+        }
+
+        if (balance < 0)
+        {
+            throw new Exception("Saldo não pode ser negativo.");
+        }
+
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string value)
+    {
+        name = value;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        balance += amount;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Bank Account - Simulação de Conta Bancária em C#
+
+Este repositório é um projeto simples para estudos iniciais em C#, simulando uma conta bancária com funcionalidades básicas. 
+
+## Objeto de estudo
+
+- Classes em C#;
+- Interfaces;
+- Sintaxe;
+
+## Funcionalidades
+- Criar uma conta bancária com nome e saldo inicial.
+- Depositar valores na conta.
+- Consultar saldo.
+
+Requisitos
+
+- .NET SDK instalado;
+- Editor de código como Visual Studio Code ou Visual Studio;
