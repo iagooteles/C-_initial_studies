61dc94861b20b4c671d25e94a9dd96d0e799a9c8
iagooteles <iagooteles@gmail.com>
2025-03-10 17:02:44 -0300

    ongoing studies

--- Program.cs
+++ Program.cs
@@ -1,32 +1,84 @@
 ﻿
-BankAccount account1 = new BankAccount("iago", 150);
-BankAccount account2 = new BankAccount("bilas", 3000);
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
 
-Console.WriteLine("Saldo antes do deposito: " + account1.Balance);
-account1.Deposit(300);
-Console.WriteLine("Saldo depois do deposito: " + account2.Balance);
+ILogger logger = new FileLogger("myPath");
+BankAccount account1 = new BankAccount("iago", 150, logger);
+BankAccount account2 = new BankAccount("bilas", 3000, logger);
 
-class ConsoleLogger
+// Console.WriteLine("Saldo antes do deposito: " + account1.Balance);
+// account1.Deposit(300);
+// account1.Deposit(-300);
+// Console.WriteLine("Saldo depois do deposito: " + account2.Balance);
+
+List<BankAccount> accounts = new List<BankAccount>();
+accounts.Add(account1);
+accounts.Add(account2);
+
+// foreach (var account in accounts) 
+// {
+//     Console.WriteLine(account.GetName());
+// }
+
+DataStore<int> store = new DataStore<int>();
+store.Value = 32;
+Console.WriteLine(store.Value);
+
+var calculate = new Calculate(Sum);
+var result = calculate(10, 20);
+Console.WriteLine(result);
+
+static int Sum(int a, int b) 
 {
-    
+    return a + b;
+}
+
+delegate int Calculate(int x, int y);
+
+class DataStore<T>
+{
+    public T Value { get; set; }
+}
+
+class FileLogger : ILogger
+{
+
+    private readonly string filePath;
+    public FileLogger(string filePath) 
+    {
+        this.filePath = filePath;
+    }
+
+    public void Log(string message)
+    {
+        File.AppendAllText(filePath + ".txt", $"{message}{Environment.NewLine}");
+    }
+}
+
+class ConsoleLogger : ILogger
+{
 }
 
 interface ILogger
 {
-    void Log(string message);
+    void Log(string message)
+    {
+        Console.WriteLine($"LOGGER: {message}");
+    }
 }
 
 class BankAccount 
 {
     private string name;
     private decimal balance;
+    private readonly ILogger logger;
 
     public decimal Balance 
     { 
         get { return balance; }
     }
 
-    public BankAccount(string name, decimal balance) {
+    public BankAccount(string name, decimal balance, ILogger logger) {
         if (string.IsNullOrWhiteSpace(name)) 
         {
             throw new Exception("Nome invalido.");
@@ -39,6 +91,7 @@ class BankAccount
 
         this.name = name;
         this.balance = balance;
+        this.logger = logger;
     }
 
     public string GetName()
@@ -53,7 +106,11 @@ class BankAccount
 
     public void Deposit(decimal amount)
     {
+        if (amount <= 0) 
+        {
+            logger.Log($"Não é possível depositar valor menor ou igual a 0.");
+            return;
+        }
         balance += amount;
     }
-
 }
--- README.md
+++ README.md
@@ -12,10 +12,18 @@ Este repositório é um projeto simples para estudos iniciais em C#, simulando u
 - Criar uma conta bancária com nome e saldo inicial.
 - Depositar valores na conta.
 - Consultar saldo.
+- FileLogger para feedback de mensagem; Cria o arquivo log.txt e registra os logs;
+- ConsoleLogger para feedback de mensagem; feedback no próprio terminal;
+- Outras funcionalidades gerais para estudos iniciais de C#;
 
-Requisitos
+## Requisitos
 
 - .NET SDK instalado;
 - Editor de código como Visual Studio Code ou Visual Studio;
 
-2:11:50
+## Objetos de estudo com C#:
+
+- ASP.NET;
+- dot.net;
+
+2:37:00
